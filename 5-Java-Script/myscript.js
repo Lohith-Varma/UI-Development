9710a2e202d7d20bbb5914c11842b26d36582986
Lohith-varma <dklohithvarma@gmail.com>
2026-06-04 08:28:50 +0530

    Initial commit

---
+++ 5-Java-Script/myscript.js
@@ -0,0 +1,7 @@
+window.document.write("Welcome to JavaScript Programming.. <br>")
+document.write("Hello JavaScript.. <br>")
+let a1 = 10
+let a2 = 20
+let sum = a1 + a2;
+document.write(" <br>Sum of two values: ",sum);
+console.log("Sum of two values : ",sum);
